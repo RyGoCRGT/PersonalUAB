529d910e1c42b60641f0fdc08332f36656f6c79f
rygopoma <rodrigopoma8@gmail.com>
2017-06-07 00:08:27 -0400

    Tipo de Usuario / Insertar /Listar

--- db/PersonalInsertar.sql
+++ db/PersonalInsertar.sql
@@ -14,6 +14,10 @@ INSERT INTO persona VALUE(null, "Rodrigo", "Luis", "Poma", "Mollo", "447457451",
 INSERT INTO persona VALUE(null, "Gustavo", "Angel", "Cerezo", "Mamani", "7417215", 1, "1995-03-30", 'M', 1);
 
 INSERT INTO tipoUsuario VALUE(null, "Administrador");
+INSERT INTO tipoUsuario VALUE(null, "Asistente");
+INSERT INTO tipoUsuario VALUE(null, "Docente");
+INSERT INTO tipoUsuario VALUE(null, "Profesor");
+
 
 INSERT INTO usuario VALUE(null, 1, 1, "rodrigo", "rodrigo", 1, 0);
 INSERT INTO usuario VALUE(null, 2, 1, "gustavo", "gustavo", 1, 0);
